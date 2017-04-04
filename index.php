73b4e68e490f893878e8da13ed4d7a873ff10bdc
Peter 4A5A <498883@edu.rocmn.nl>
2017-04-04 13:53:57 +0200

    Changed the onload

--- index.php
+++ index.php
@@ -15,11 +15,11 @@
   <script src="main.js"></script>
 
   </head>
-  <body onload='getRequest("getTable"); getPages();'>
+  <body onload='GoToPage("0"); getPages();'>
     <div class="row">
       <button class='col-2' onclick=getRequest('createFormForProduct'); type='button'>Create product</button>
       <div class="col-4">
-        <input type="text" id="search" placeholder="Zoek op: productnaam en details" oninput="searchItem(this);"><button type="button" onclick="searchItem(this);">Zoeken</button>
+        <input type="text" id="search" placeholder="Zoek op: productnaam en details"><button type="button" onclick="searchItem(search);">Zoeken</button>
       </div>
       <div class="col-6"></div>
       <div id="content">
